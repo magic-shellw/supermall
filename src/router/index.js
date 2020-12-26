6e88cd4e3592e6e096e91612c680721f0e079462
magic-shellw <2177065844@qq.com>
2020-12-26 14:39:38 +0800

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -35,10 +35,10 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  /* scrollBehavior(to, from, savedPosition) {
     if(savedPosition) return savedPosition;
     return {x: 0, y: 0}
-  }
+  } */
 });
 
 export default router;
